971fcfbaad0ea0c31110ac65bf2328e1b4d0825a
Coleman Irby <ckirbyee92@gmail.com>
2026-04-07 13:46:32 -0500

    remove old dir

--- fangorn-data-discovery/packages/client/graphql.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  // a path to a local `.graphql` file
-  schema: './src/schema.graphql',
-	documents: './src/queries/**/*.graphql'
-}
--- fangorn-data-discovery/packages/client/src/queries.ts
+++
@@ -1,4 +0,0 @@
-
-export const getManifestsbySchema = `
-query get whatever (where: {name: $NAME})
-`
