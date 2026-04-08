99810f26e4f285c41c6490c42a532cfc806b7494
Coleman Irby <ckirbyee92@gmail.com>
2026-04-07 22:01:32 -0500

    Changed project structure

--- packages/client/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-import { getSdk } from "./client.js";
-export function getSubgraphClients(url) {
-    const client = new GraphQLClient(url);
-    const typedClient = getSdk(client);
-    return { client, typedClient };
-}
-//# sourceMappingURL=index.js.map
