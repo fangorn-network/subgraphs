70e6574b70e2eb66f5acd4eda8f8be07cd0fdf35
Coleman Irby <ckirbyee92@gmail.com>
2026-04-09 14:53:39 -0500

    fix formatting

--- client-types/src/index.ts
+++ client-types/src/index.ts
@@ -1,70 +1,70 @@
 export interface SchemaState {
-  id: string
-  schemaId: string
-  owner: string
-  name: string
-  versions?: Schema[]
+	id: string
+	schemaId: string
+	owner: string
+	name: string
+	versions?: Schema[]
 }
 
 export interface Schema {
-  id: string
-  manifests?: ManifestState[]
-  version?: String
-  agentId?: String
-  fields?: SchemaField[]
+	id: string
+	manifests?: ManifestState[]
+	version?: String
+	agentId?: String
+	fields?: SchemaField[]
 }
 
 export interface SchemaField {
-  id: string;
-  name: string;
-  fieldType: string;
+	id: string;
+	name: string;
+	fieldType: string;
 }
 
 export interface ManifestState {
-  id: string
+	id: string
 	schemaId: string
-  schemaName: string
-  owner: string
-  manifestCid: string
-  manifest?: Manifest
-  version: string
-  lastUpdated: string
+	schemaName: string
+	owner: string
+	manifestCid: string
+	manifest?: Manifest
+	version: string
+	lastUpdated: string
 }
 
 export interface Manifest {
-  id: string
+	id: string
 	schemaId: string
-  schemaName: string
+	schemaName: string
 	manifestStateId: string
-  manifestVersion?: string
-  files?: FileEntry[]
+	manifestVersion?: string
+	files?: FileEntry[]
 }
 
 export interface FileEntry {
-  id: string
+	id: string
 	schemaId: string
-  schemaName: string
+	schemaName: string
 	manifestStateId: string
-  tag?: string
-  fileFields?: FileField[]
+	tag?: string
+	fileFields?: FileField[]
 }
 
 export interface FileField {
-  id: string
+	id: string
 	schemaId: string
-  schemaName: string
+	schemaName: string
 	manifestStateId: string
 	fileId: string
-  name?: string
-  value?: string
-  atType?: string
-  acc?: string
-  pricing?: PricingResource
+	name?: string
+	value?: string
+	atType?: string
+	acc?: string
+	pricing?: PricingResource
 }
 
 export interface PricingResource {
-  id: string;
-  owner: string;
-  price: string;
-  currency: string;
+	id: string;
+	owner: string;
+	price: string;
+	currency: string;
 }
